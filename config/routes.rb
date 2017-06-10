44843f1a6c88880238461fdb26c5492f532fc208
Misael Bejarano <vonleny@gmail.com>
2017-06-10 14:10:48 -0400

    spike out signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resource :users
 
   get 'home/index'
   root 'home#index'
-  get 'home/login'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
